49249dceaa8212cf1cd6eb45a9a34499374b6879
gencturkemre <you@example.com>
2022-11-02 00:00:12 +0300

    four commit

---
+++ src/gitTekrarcalismasi_01/GitTekrarCalismasi.java
@@ -0,0 +1,10 @@
+package gitTekrarcalismasi_01;
+
+public class GitTekrarCalismasi {
+    public static void main(String[] args) {
+        System.out.println("emre");
+        System.out.println("git e yeni kod ekle");
+        System.out.println("son bölüm");
+    }
+
+}
